10e8b568d0922d6b0b6f564c458a53ab5aabcd63
AnshulShukla136 <anshulshukla136@gmail.com>
2026-03-24 22:46:51 +0530

    changes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
->>>>>>> e70952e8a0156c54c82f98ccd2ae6a5f57c76b20
 }
